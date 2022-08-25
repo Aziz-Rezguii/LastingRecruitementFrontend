c1ef10582c47425c29394b72118ba75da7af91f1
Rezgui Aziz <rezgui01aziz@gmail.com>
2022-08-25 06:54:07 +0100

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:"http://localhost:5000/api/v1",
+    baseURL:"https://lastingrecruitement-backend.herokuapp.com/api/v1/jobs",
     headers:{
         "Content-type":"application/json"
     }
