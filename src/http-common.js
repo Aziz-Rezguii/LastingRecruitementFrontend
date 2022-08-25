313db780a442ea402d4fc581d552705b774db45e
Rezgui Aziz <rezgui01aziz@gmail.com>
2022-08-25 06:55:41 +0100

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:"https://lastingrecruitement-backend.herokuapp.com/api/v1/jobs",
+    baseURL:"https://lastingrecruitement-backend.herokuapp.com/api/v1/",
     headers:{
         "Content-type":"application/json"
     }
