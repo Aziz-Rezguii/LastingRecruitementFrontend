ca693145cc2e0452617df9af19b3fc284fdaa90d
Rezgui Aziz <101634616+Aziz-Rezguii@users.noreply.github.com>
2023-12-20 16:41:30 +0100

    Update joblist.js

--- src/components/joblist.js
+++ src/components/joblist.js
@@ -1,302 +1,229 @@
 import React, { useState, useEffect } from "react";
-import JobsDataService from "../services/jobs";
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { styled } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Grid";
 import { useNavigate } from "react-router-dom";
-import MoonLoader from "react-spinners/ClipLoader";
-import SearchIcon from '@mui/icons-material/Search';
+import SearchIcon from "@mui/icons-material/Search";
 
-const Item = styled('div')(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+const Item = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
+
 function getWindowSize() {
-  const {innerWidth, innerHeight} = window;
-  return {innerWidth, innerHeight};
+  const { innerWidth, innerHeight } = window;
+  return { innerWidth, innerHeight };
 }
 
-const JobsList = props => {
+const JobsList = (props) => {
   const [windowSize, setWindowSize] = useState(getWindowSize());
-  const [Jobs, setJobs] = useState([]);
-  const [searchName, setSearchName ] = useState("");
-  const [Loading, setLoading ] = useState(false);
-  
-
+  const [Jobs, setJobs] = useState([
+    {
+      _id: "1",
+      name: "Sample Job",
+      payrate: "$20 per hour",
+      industry: "Sample Industry",
+      address: "Sample Address",
+      jobduties: "Sample Job Duties",
+      shifts: "Sample Shifts",
+      date: "2023-12-20",
+      jobrequirements: "Sample Job Requirements",
+    },
+  ]);
+  const [searchName, setSearchName] = useState("");
 
   useEffect(() => {
-    retrieveJobs();
-    setLoading(true)
     function handleWindowResize() {
       setWindowSize(getWindowSize());
     }
 
-    window.addEventListener('resize', handleWindowResize);
+    window.addEventListener("resize", handleWindowResize);
 
     return () => {
-      window.removeEventListener('resize', handleWindowResize);
+      window.removeEventListener("resize", handleWindowResize);
     };
   }, []);
 
-  const onChangeSearchName = e => {
+  const onChangeSearchName = (e) => {
     const searchName = e.target.value;
     setSearchName(searchName);
   };
 
-
-  const retrieveJobs = () => {
-    JobsDataService.getAll()
-      .then(response => {
-        setJobs(response.data.jobs);
-        console.log(response.data.jobs);
-        setLoading(false)
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  };
-
-
-
   const navigate = useNavigate();
-  if(windowSize.innerWidth>650){
+
   return (
     <div>
-      <Box
-            component="form"
-            sx={{'& > :not(style)': { m: 1, width: '25ch',height:'5ch' },}}
-            noValidate
-            autoComplete="off"
-            >
-              <div></div>
-          </Box>
-          <div className="row pb-1">
-          <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 10, md: -10 }}>
+      <div className="row pb-1">
+        <Grid
+          container
+          rowSpacing={1}
+          columnSpacing={{ xs: 1, sm: 10, md: -10 }}
+        >
           <Grid item xs={6}>
-          <Item>
-            <div className="input-group col-lg-4">
-            <h2  style={{color:'#5BB462',paddingLeft:20}}> <SearchIcon/> Find Jobs Faster</h2>
-              <Box
-                component="form"
-                sx={{'& > :not(style)': { m: 1, width: '25ch',height:'4ch'},}}
-                noValidate
-                autoComplete="off"
-                >
-                  
-                
-                <TextField style={{marginLeft:80}} id="outlined-basic" label="Search by Name" variant="outlined" value={searchName} onChange={onChangeSearchName}/></Box>
-              <div className="input-group-append">
+            <Item>
+              <div className="input-group col-lg-4">
+                <h2 style={{ color: "#5BB462", paddingLeft: 20 }}>
+                  {" "}
+                  <SearchIcon /> Find Jobs Faster
+                </h2>
                 <Box
-                component="form"
-                sx={{'& > :not(style)': { m: 1, width: '25ch',height:'1ch' },}}
-                noValidate
-                autoComplete="off"
+                  component="form"
+                  sx={{
+                    "& > :not(style)": {
+                      m: 1,
+                      width: "25ch",
+                      height: "4ch",
+                    },
+                  }}
+                  noValidate
+                  autoComplete="off"
                 >
-                  <div></div>
-              </Box>
+                  <TextField
+                    style={{ marginLeft: 80 }}
+                    id="outlined-basic"
+                    label="Search by Name"
+                    variant="outlined"
+                    value={searchName}
+                    onChange={onChangeSearchName}
+                  />
+                </Box>
+                <div className="input-group-append">
+                  <Box
+                    component="form"
+                    sx={{
+                      "& > :not(style)": {
+                        m: 1,
+                        width: "25ch",
+                        height: "1ch",
+                      },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                  >
+                    <div></div>
+                  </Box>
+                </div>
               </div>
-            </div>
             </Item>
-            </Grid>
-            <Grid item xs={6}>
-          <Item>
-            <div className="input-group col-lg-4">
-          <div className="input-group-append">
-            <Box
-            component="form"
-            sx={{'& > :not(style)': { m: 1, width: '25ch',height:'1ch' },}}
-            noValidate
-            autoComplete="off"
-            >
-              <div></div>
-            </Box>
-            </div>
-          </div>
-          </Item>
           </Grid>
+          <Grid item xs={6}>
+            <Item>
+              <div className="input-group col-lg-4">
+                <div className="input-group-append">
+                  <Box
+                    component="form"
+                    sx={{
+                      "& > :not(style)": {
+                        m: 1,
+                        width: "25ch",
+                        height: "1ch",
+                      },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                  >
+                    <div></div>
+                  </Box>
+                </div>
+              </div>
+            </Item>
           </Grid>
-        </div>
-      <div className="row">
-        {
-          Loading ?
-          <div style={{
-            paddingLeft:(windowSize.innerWidth/2)-100}}>
-          <MoonLoader size={100}  />
-          </div>
-          :
-          <TableContainer sx={{paddingLeft:20,paddingRight:20,paddingBottom:20}} component={'div'}>
-      <Table sx={{padding:20 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-          <TableCell><h1>Jobs</h1></TableCell>
-            <TableCell align="right"></TableCell>
-            <TableCell align="right"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {Jobs.filter((Jobs)=>{
-            if(searchName===""){
-              return Jobs
-            }else if(Jobs.name.toLowerCase().includes(searchName.toLowerCase())){
-              return Jobs
-            }return null;
-          }).map((Jobs) => (
-            <TableRow
-              key={Jobs.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                <b>{Jobs.name}</b><br></br>
-                {Jobs.payrate}<br></br>
-                {Jobs.industry}
-              </TableCell>
-              <TableCell align="right">{Jobs.adress}</TableCell>
-              <TableCell align="right"><Button onClick={() => navigate("/MoreInfo_"+Jobs._id,{ state: { id: Jobs._id, name:Jobs.name, adress:Jobs.adress,industry:Jobs.industry, payrate:Jobs.payrate, jobduties:Jobs.jobduties, shifts:Jobs.shifts, date:Jobs.date ,jobrequirements:Jobs.jobrequirements  }})}>More Info</Button></TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      </TableContainer>
-
-        }
-      
-    <Box
-            component="form"
-            sx={{'& > :not(style)': { m: 1, width: '20ch',height: '10ch'},}}
-            noValidate
-            autoComplete="off"
-            >
-            <div></div>
-          </Box>
-
+        </Grid>
       </div>
+      <div className="row">
+        <TableContainer
+          sx={{ paddingLeft: 20, paddingRight: 20, paddingBottom: 20 }}
+          component={"div"}
+        >
+          <Table sx={{ padding: 20 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <h1>Jobs</h1>
+                </TableCell>
+                <TableCell align="right"></TableCell>
+                <TableCell align="right"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Jobs.filter((job) => {
+                if (searchName === "") {
+                  return job;
+                } else if (
+                  job.name.toLowerCase().includes(searchName.toLowerCase())
+                ) {
+                  return job;
+                }
+                return null;
+              }).map((job) => (
+                <TableRow
+                  key={job.name}
+                  sx={{
+                    "&:last-child td, &:last-child th": { border: 0 },
+                  }}
+                >
+                  <TableCell component="th" scope="row">
+                    <b>{job.name}</b>
+                    <br></br>
+                    {job.payrate}
+                    <br></br>
+                    {job.industry}
+                  </TableCell>
+                  <TableCell align="right">{job.address}</TableCell>
+                  <TableCell align="right">
+                    <Button
+                      onClick={() =>
+                        navigate("/MoreInfo_" + job._id, {
+                          state: {
+                            id: job._id,
+                            name: job.name,
+                            address: job.address,
+                            industry: job.industry,
+                            payrate: job.payrate,
+                            jobduties: job.jobduties,
+                            shifts: job.shifts,
+                            date: job.date,
+                            jobrequirements: job.jobrequirements,
+                          },
+                        })
+                      }
+                    >
+                      More Info
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": {
+            m: 1,
+            width: "20ch",
+            height: "10ch",
+          },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <div></div>
+      </Box>
     </div>
   );
-}else{
-  return (
-    <div>
-      <Box
-            component="form"
-            sx={{'& > :not(style)': { m: 1, width: '25ch',height:'5ch' },}}
-            noValidate
-            autoComplete="off"
-            >
-              <div></div>
-          </Box>
-          <div className="row pb-1" width>
-          <Grid container rowSpacing={1} columnSpacing={{ xs: 50, sm: 10, md: -50 }}>
-          <Grid item xs={6}>
-          <Item>
-            <div className="input-group col-lg-4">
-              <Box
-                component="form"
-                sx={{'& > :not(style)': { m: 1, width: '25ch',height:'4ch'},}}
-                noValidate
-                autoComplete="off"
-                >
-                  
-                <h2  style={{color:'#5BB462',paddingLeft:10}}> <SearchIcon/> Find Jobs Faster</h2>
-                <TextField style={{marginLeft:80}} id="outlined-basic" label="Search by Name" variant="outlined" value={searchName} onChange={onChangeSearchName}/>
-              </Box>
-              <div className="input-group-append">
-                <Box
-                component="form"
-                sx={{'& > :not(style)': { m: 1, width: '25ch',height:'1ch' },}}
-                noValidate
-                autoComplete="off"
-                >
-                  <div></div>
-              </Box>
-              </div>
-            </div>
-            </Item>
-            
-            </Grid>  
-            <Grid item xs={6}>
-          <Item>
-            <div className="input-group col-lg-4">
-          <div className="input-group-append">
-            <Box
-            component="form"
-            sx={{'& > :not(style)': { m: 1, width: '25ch',height:'1ch' },}}
-            noValidate
-            autoComplete="off"
-            >
-              <div></div>
-            </Box>
-            </div>
-          </div>
-          </Item>
-          </Grid>
-          </Grid>
-        </div>
-      <div className="row">
-      {
-          Loading ?
-          <div style={{
-            paddingLeft:(windowSize.innerWidth/2)-100}}>
-          <MoonLoader size={100}  />
-          </div>
-          :
-          <TableContainer component={'div'}>
-          <Table sx={{ minWidth:200,padding:20 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell><h1>Jobs</h1></TableCell>
-            <TableCell align="right"></TableCell>
-            <TableCell align="right"></TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {Jobs.filter((Jobs)=>{
-            if(searchName===""){
-              return Jobs
-            }else if(Jobs.name.toLowerCase().includes(searchName.toLowerCase())){
-              return Jobs
-            }return null;
-          }).map((Jobs) => (
-            <TableRow
-              key={Jobs.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                <b>{Jobs.name}</b><br></br>
-                {Jobs.payrate}<br></br>
-                {Jobs.industry}
-              </TableCell>
-              <TableCell align="right">{Jobs.adress}</TableCell>
-              <TableCell align="right"><Button onClick={() => navigate("/MoreInfo_"+Jobs._id,{ state: { id: Jobs._id, name:Jobs.name, adress:Jobs.adress,industry:Jobs.industry, payrate:Jobs.payrate, jobduties:Jobs.jobduties, shifts:Jobs.shifts, date:Jobs.date ,jobrequirements:Jobs.jobrequirements  }})}>More Info</Button></TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-        }
-    <Box
-            component="form"
-            sx={{'& > :not(style)': { m: 1, width: '20ch',height: '10ch'},}}
-            noValidate
-            autoComplete="off"
-            >
-            <div></div>
-          </Box>
-
-      </div>
-    </div>
-  );
-  
-}
 };
 
 export default JobsList;
